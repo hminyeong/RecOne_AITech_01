eca0693f6445c41bde38c716508962616033ece1
momey11 <momey11@skku.edu>
2022-03-06 23:05:24 +0900

    Create 6118_임경연.py

---
+++ 알고리즘스터디/6118/6118_임경연.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+import copy
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+routes = [[] for _ in range(N)]
+
+for i in range(M):
+    x, y = map(int, input().split())
+    routes[x-1].append(y-1)
+    routes[y-1].append(x-1)
+
+visited = [0] * N
+
+queue = deque()
+
+visited[0] = 1
+
+queue.append(0)
+visited[0] = 1
+
+while queue:
+    node = queue.popleft()
+
+    for i in routes[node]:
+        if visited[i] == 0:
+            visited[i] = visited[node] + 1
+            queue.append(i)
+
+
+a = max(visited)
+b = visited.index(a) + 1
+c = visited.count(a)
+
+print(b, a - 1, c)
+
