e625a460fce6e79c49ee2a3e0474c93aa35d6ed2
djh0211 <tnfxoddl02@gmail.com>
2022-03-07 09:50:58 +0900

    Create 14499_하동준.py

---
+++ 알고리즘스터디/14499/14499_하동준.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+# 배열선언
+# 상,하,좌,우,반대 순
+
+# 0 인덱스 안쓸거
+flat_rule = []
+flat_rule.append('NOT USE')
+tmp = {}
+tmp['left'] = 4
+tmp['right'] = 3
+tmp['up'] = 2
+tmp['down'] = 5 
+tmp['opp'] = 6
+
+tmp = {}
+tmp['left'] = 4
+tmp['right'] = 3
+tmp['up'] = 2
+tmp['down'] = 5 
+tmp['opp'] = 6
+
+tmp = {}
+tmp['left'] = 4
+tmp['right'] = 3
+tmp['up'] = 2
+tmp['down'] = 5 
+tmp['opp'] = 6
+
+
