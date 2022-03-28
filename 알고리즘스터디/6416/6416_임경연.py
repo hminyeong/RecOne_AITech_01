608e478ee02ecaa6729ea1e58be079ea75d9a838
momey11 <momey11@skku.edu>
2022-03-28 19:13:06 +0900

    Create 6416_임경연.py

--- 알고리즘스터디/6416/6416_임경연.py
+++ 알고리즘스터디/6416/6416_임경연.py
@@ -20,13 +20,15 @@ while True:
         continue
     inputs = inputs + input_
 
-
+tree_ = defaultdict(list)
 for tree in trees:
     l = trees[tree]
     token = 0
     parent = []
     child = []
-    root = "None"
+    if len(l) == 2:
+        print(f"Case {tree} is a tree.")
+        continue
     while True:
         x = l.pop(0)
         y = l.pop(0)
@@ -37,16 +39,16 @@ for tree in trees:
         else:
             token = 1
             break
-        if root == "None" and x not in parent:
+        tree_[x].append(y)
+        if x not in parent:
             parent.append(x)
-        elif root == "None":
-            parent.append(x)
-            root = x
-        elif root != x:
-            print(tree, x, y)
-            token = 1
-            break
-    if token == 0:
-        print(f"Case {tree} is a tree.")
+    root = 0
+    for p in parent:
+        if p not in child:
+            root += 1
+    if root != 1:
+        token = 1
+    if token == 1:
+        print(f"Case {tree} is not a tree.")            
     else:
-        print(f"Case {tree} is not a tree.")
+        print(f"Case {tree} is a tree.")
